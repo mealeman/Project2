1ddfb36345184d0ee3e700a0a5914bcc9d2242d1
mealeman <mealeman@csumb.edu>
2023-02-07 17:53:00 -0800

    Updated Schema

--- Project2DB.sql
+++ Project2DB.sql
@@ -7,6 +7,7 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 -- -----------------------------------------------------
 -- Schema mydb
 -- -----------------------------------------------------
+DROP SCHEMA IF exists `mydb`;
 
 -- -----------------------------------------------------
 -- Schema mydb
@@ -28,11 +29,12 @@ ENGINE = InnoDB;
 -- Table `mydb`.`Pharmacy`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `mydb`.`Pharmacy` (
+  `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(45) NOT NULL,
   `address` VARCHAR(45) NOT NULL,
   `phone` VARCHAR(45) NOT NULL,
   `supervisor` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`name`))
+  PRIMARY KEY (`id`))
 ENGINE = InnoDB;
 
 
@@ -45,14 +47,14 @@ CREATE TABLE IF NOT EXISTS `mydb`.`Contract` (
   `startdate` DATE NOT NULL,
   `enddate` DATE NOT NULL,
   `contracttext` VARCHAR(500) NOT NULL,
-  `Pharmacy_name` VARCHAR(45) NOT NULL,
+  `Pharmacy_id` INT NOT NULL,
   `PharmaceuticalCompany_Pharm Co name` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`contractid`, `Pharmacy_name`, `PharmaceuticalCompany_Pharm Co name`),
-  INDEX `fk_Contract_Pharmacy1_idx` (`Pharmacy_name` ASC) VISIBLE,
+  PRIMARY KEY (`contractid`, `Pharmacy_id`, `PharmaceuticalCompany_Pharm Co name`),
+  INDEX `fk_Contract_Pharmacy1_idx` (`Pharmacy_id` ASC) VISIBLE,
   INDEX `fk_Contract_PharmaceuticalCompany1_idx` (`PharmaceuticalCompany_Pharm Co name` ASC) VISIBLE,
   CONSTRAINT `fk_Contract_Pharmacy1`
-    FOREIGN KEY (`Pharmacy_name`)
-    REFERENCES `mydb`.`Pharmacy` (`name`)
+    FOREIGN KEY (`Pharmacy_id`)
+    REFERENCES `mydb`.`Pharmacy` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Contract_PharmaceuticalCompany1`
@@ -125,7 +127,7 @@ CREATE TABLE IF NOT EXISTS `mydb`.`Prescription` (
   `date` DATE NULL,
   `quantity` INT UNSIGNED NOT NULL,
   `price` INT UNSIGNED NULL,
-  `pharmacy` VARCHAR(45) NULL,
+  `pharmacy` INT NULL,
   `company` VARCHAR(45) NULL,
   `doctor` INT NOT NULL,
   `patient` INT UNSIGNED NOT NULL,
@@ -147,7 +149,7 @@ CREATE TABLE IF NOT EXISTS `mydb`.`Prescription` (
     ON UPDATE NO ACTION,
   CONSTRAINT `prescription.pharmacy`
     FOREIGN KEY (`pharmacy`)
-    REFERENCES `mydb`.`Pharmacy` (`name`)
+    REFERENCES `mydb`.`Pharmacy` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `prescription.company`
@@ -167,14 +169,14 @@ ENGINE = InnoDB;
 -- Table `mydb`.`Pharmacy_sells_Drug`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `mydb`.`Pharmacy_sells_Drug` (
-  `Pharmacy_name` VARCHAR(45) NOT NULL,
+  `Pharmacy_id` INT NOT NULL,
   `drug_drug_id` INT(11) NOT NULL,
   `drug_Pharmacy_sells_Drug_pharmacy_id` INT NOT NULL,
-  PRIMARY KEY (`Pharmacy_name`, `drug_drug_id`, `drug_Pharmacy_sells_Drug_pharmacy_id`),
+  PRIMARY KEY (`Pharmacy_id`, `drug_drug_id`, `drug_Pharmacy_sells_Drug_pharmacy_id`),
   INDEX `fk_Pharmacy_sells_Drug_drug1_idx` (`drug_drug_id` ASC, `drug_Pharmacy_sells_Drug_pharmacy_id` ASC) VISIBLE,
   CONSTRAINT `fk_Pharmacy_sells_Drug_Pharmacy1`
-    FOREIGN KEY (`Pharmacy_name`)
-    REFERENCES `mydb`.`Pharmacy` (`name`)
+    FOREIGN KEY (`Pharmacy_id`)
+    REFERENCES `mydb`.`Pharmacy` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Pharmacy_sells_Drug_drug1`
